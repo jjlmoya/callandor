f534d428aa243bc36a9a1feb6b2dcd75b533588e
jjlmoya <juanjo1989@gmail.com>
2019-10-11 14:33:08 +0200

    Add comic layout

---
+++ src/layout/Comic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="l-comic l-comic--dark">
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+    <div class="l-comic__item"></div>
+  </div>
+</template>
+<style lang="scss">
+  @import 'comic.scss';
+</style>
+
