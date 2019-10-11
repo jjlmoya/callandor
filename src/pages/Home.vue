5ff1e14e214642d19e119f813d3d2b5db6c4db4c
jjlmoya <juanjo1989@gmail.com>
2019-10-11 14:35:00 +0200

    Set Home Components

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,37 @@
 <template>
-  <Layout>
-    <div class="home">
-      Home
-    </div>
-  </Layout>
+  <div>
+    <Layout>
+      <Typing>
+        La Rueda del tiempo gira y las eras llegan y pasan y dejan tras de sí recuerdos que se convierten en
+        leyenda. La leyenda se difumina, deviene mito, e incluso el mito se ha olvidado mucho antes de que la era que lo
+        vio nacer retorne de nuevo. En una era llamada la tercera por algunos, una era que ha de venir, una era
+        transcurrida hace mucho, comenzó a soplar un viento. El viento no fue el principio, pues no existen comienzos ni
+        finales en el eterno girar de la Rueda del Tiempo. Pero aquél fue un principio.
+      </Typing>
+    </Layout>
+    <Comic></Comic>
+  </div>
+
+
 </template>
 
 
 <script>
+  import Comic from '@/layout/Comic.vue';
   import Layout from '@/layout/Default.vue';
-  import SeoService from "../services/seoService";
+  import Typing from '@/components/Typing/Typing.vue';
+  import SeoService from '../services/seoService';
 
   export default {
     name: 'home',
     components: {
-      Layout
+      Layout,
+      Typing,
+      Comic
     },
     metaInfo() {
       let seoService = new SeoService({
-        title: 'Home',
+        title: 'La Rueda del Tiempo',
         description: 'This is a description',
         image: 'this-is-image.png',
         richData: [
