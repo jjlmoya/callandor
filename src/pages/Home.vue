a69268c569fa7156ab67b8f265b960f5663c2aea
jjlmoya <juanjo1989@gmail.com>
2019-10-13 08:31:16 +0200

    Typing Effect

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <Layout>
-      <Typing>
-        La Rueda del tiempo gira y las eras llegan y pasan y dejan tras de sí recuerdos que se convierten en
+      <Typing toType=" La Rueda del tiempo gira y las eras llegan y pasan y dejan tras de sí recuerdos que se convierten en
         leyenda. La leyenda se difumina, deviene mito, e incluso el mito se ha olvidado mucho antes de que la era que lo
         vio nacer retorne de nuevo. En una era llamada la tercera por algunos, una era que ha de venir, una era
         transcurrida hace mucho, comenzó a soplar un viento. El viento no fue el principio, pues no existen comienzos ni
-        finales en el eterno girar de la Rueda del Tiempo. Pero aquél fue un principio.
-      </Typing>
+        finales en el eterno girar de la Rueda del Tiempo. Pero aquél fue un principio."/>
       <Comic></Comic>
     </Layout>
 
